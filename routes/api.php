98edccd26aaa9ec8c51c0b09c3444cefbc39a45a
darik aflah <darikaflah@gmail.com>
2022-11-01 22:01:53 +0700

    Auth

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiswaController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthController;
 
 
 /*
@@ -19,10 +20,20 @@ use App\Http\Controllers\BookController;
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
-
-// Route::get('halo', function () {
-//     return ["me"=> "Guanteng"];
 // });
-// Route::get('cuy', [SiswaController::class, 'index']);
-Route::apiResource('siswa', SiswaController::class);
-Route::apiResource('books', BookController::class);
+
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{id}', [BookController::class, 'show']);
+
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::resource('books', BookController::class)->except('create', 'edit', 'show', 'index');
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
